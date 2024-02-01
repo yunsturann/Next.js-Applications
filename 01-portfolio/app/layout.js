a56a5c5dc7efc1a5f64777f05f3228fad80c5dd1
Yunus <ynstrn811@gmail.com>
2024-02-01 03:23:36 +0300

    speed insights

--- 01-portfolio/app/layout.js
+++ 01-portfolio/app/layout.js
@@ -1,5 +1,6 @@
 import { Gemunu_Libre } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 import Header from "@/components/Header";
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
