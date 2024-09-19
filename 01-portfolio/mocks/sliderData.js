420404275533cc1f3236c37d1a2956e86d2eac2b
Yunus <ynstrn811@gmail.com>
2024-09-19 20:55:09 +0300

    fix(meta-description) add a new seo-enhancing meta-description

--- 01-portfolio/mocks/sliderData.js
+++ 01-portfolio/mocks/sliderData.js
@@ -1,10 +1,10 @@
 // data for testimonials slider it will have array of objects contains name and image and text
 
 import me from "../public/assets/images/meCroped.webp";
-import dilber from "../public/assets/images/dilber.jpg";
 import oguz from "../public/assets/images/oguz.png";
 import umut from "../public/assets/images/umut.jpg";
 import corut from "@/public/assets/images/corut.jpg";
+import dilber from "../public/assets/images/dilber.jpg";
 
 export const sliderData = [
   {
@@ -16,30 +16,30 @@ export const sliderData = [
   },
   {
     id: 2,
-    name: "Dilber Soydaş",
-    image: dilber,
-    text: "I learned a lot from his github. Also, He is such a good person.",
-    role: "Public Relations Manager",
-  },
-  {
-    id: 3,
     name: "Oğuzhan Özdemir",
     image: oguz,
     text: "He is a good friend of mine. He is always ready to help. I am glad to know him.",
     role: "Flutter Developer",
   },
   {
-    id: 4,
+    id: 3,
     name: "Umut Şahin",
     image: umut,
     text: "I am working with him. He is the best team player I have ever met.",
     role: "Backend Developer",
   },
   {
-    id: 5,
+    id: 4,
     name: "Corut Ali",
     image: corut,
     text: "He set up a webpage for me. It is pretty fancy and very fast. Nice job!",
     role: "No Stack Developer",
+  },
+  {
+    id: 5,
+    name: "Dilber Soydaş",
+    image: dilber,
+    text: "I learned a lot from his github. Also, He is such a good person.",
+    role: "Public Relations Manager",
   },
 ];
